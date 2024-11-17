a1faac43b8c5425dce9a761652ec8c2c432e8a09
PhiloAtef <philoatefdev@gmail.com>
2024-11-17 13:38:01 +0200

    start of new lesson

--- index.php
+++ index.php
@@ -1,48 +1,5 @@
 <?php
 
-//string comparisons are determined by what first letter comes first in the alphabet
-//uppercase letters are seen as less than lower case letters
-//loose equality and strict equality
-//loose is == where it doesnt look at the type of data
-//strict is === where it takes into account the type of data
-
-//5 == '5' is 1 
-//5 === '5' is ""
-
-//functions
-//==========
-
-function sayHello($name = 'anonymous'){
-    echo "helloooooo everyyyyoneeee. especially $name! <br />";
-}
-
-sayHello('philo');
-
-function formatProduct($product){
-    echo "{$product['name']} costs {$product['price']} to buy <br />";
-}
-
-formatProduct(['name'=>'gold star','price'=> 20]);
-
-function formatProductWithReturn($product){
-    return "{$product['name']} costs {$product['price']} to buy but formatted <br />";
-}
-
-$formatted = formatProductWithReturn(['name'=>'gold star','price'=> 20]);
-
-echo $formatted;
-
-//variable scopes
-
-//variables can be local and global
-//inside function variables cant be called from outside function
-//using 
-global $name;
-//sets a variable as global
-//you can change global variables inside functions to pass data to another function
-//adding & before variable passes it by reference
-
-
 ?>
 
 <!DOCTYPE html>
