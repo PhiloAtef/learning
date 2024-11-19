f04bd3451237cc11c4f712acefee1bbd7d1b960e
PhiloAtef <philoatefdev@gmail.com>
2024-11-19 11:54:31 +0200

    errors array

--- add.php
+++ add.php
@@ -1,5 +1,7 @@
 <?php
 
+$errors = array('email'=>'','title'=>'','ingredients'=>'');
+
 if(isset($_POST['submit'])){
 
     //form validation 
@@ -9,7 +11,7 @@ if(isset($_POST['submit'])){
     }else{
         $email = $_POST['email'];
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            echo 'email must be a valid email address';
+            $errors['email'] = 'email must be a valid email address';
         }
 
     }
@@ -19,7 +21,7 @@ if(isset($_POST['submit'])){
     }else{
         $title = $_POST['title'];
         if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-            echo 'Title must be letters and spaces only';
+            $errors['title'] = 'Title must be letters and spaces only';
         }
     }
     //check ingredients 
@@ -28,7 +30,7 @@ if(isset($_POST['submit'])){
     }else{
         $ingredients = $_POST['ingredients'];
         if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$ingredients)){
-            echo 'ingredients must be in a comma seperated list' ;
+            $errors['ingredients']= 'ingredients must be in a comma seperated list' ;
         }
     }
     //end of post check
