2a19594bf3fae14b29719aa8312a672cd2c82fa8
PhiloAtef <philoatefdev@gmail.com>
2024-11-20 08:29:11 +0200

    open close connection and fetching results

--- index.php
+++ index.php
@@ -6,6 +6,21 @@ if(!$conn){
     echo 'connection error: '.mysqli_connect_error();
 }
 
+//write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+//make query and get results
+$results = mysqli_query($conn,$sql);
+
+//fetch the resulting rows as an array 
+$pizzas = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+print_r($pizzas);
+
+mysqli_free_result($results);
+
+//close connection 
+mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
