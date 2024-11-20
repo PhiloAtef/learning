94d3b06856baae19c70f5f276e2c12d48271a4dc
PhiloAtef <philoatefdev@gmail.com>
2024-11-20 10:03:03 +0200

    displaying fetched data

--- index.php
+++ index.php
@@ -7,15 +7,13 @@ if(!$conn){
 }
 
 //write query for all pizzas
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 //make query and get results
 $results = mysqli_query($conn,$sql);
 
 //fetch the resulting rows as an array 
 $pizzas = mysqli_fetch_all($results, MYSQLI_ASSOC);
-
-print_r($pizzas);
 
 mysqli_free_result($results);
 
@@ -27,6 +25,26 @@ mysqli_close($conn);
 <html lang="en">
 <?php include('templates/header.php') ?>
 
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+    <div class="row">
+        <?php foreach($pizzas as $pizza){ ?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title'])?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients'])?></div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">More Info</a>
+                    </div>
+                </div>
+            </div>
+        
+        <?php }?>
+    </div>
+</div>
 <?php include('templates/footer.php') ?>   
 
 </html>
